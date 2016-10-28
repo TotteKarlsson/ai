65d762de53c7d06e8c47c3b954f795d07e95b1b4
Totte Karlsson <v-matsk@alleninstitute.org>
2016-10-28 16:11:15 -0700

    Added python app

---
+++ python/coverslipdustassay/DustAssayApp.py
@@ -0,0 +1,121 @@
+from PyQt4 import QtCore, QtGui, uic
+import os
+import sys
+import pyqtgraph as pg
+import numpy as np
+import MMCorePy
+import mysql.connector
+from datetime import datetime
+
+class MyWidget(QtGui.QWidget):
+    def __init__(self,configfile=''):
+        QtGui.QWidget.__init__(self)
+        self.initUI()
+        self.pushButton1.clicked.connect(self.dopush1)
+        self.pushButton2.clicked.connect(self.dopush2)
+        self.PopulateBtn.clicked.connect(self.saveBtnClick)
+        self.mmc = MMCorePy.CMMCore()
+        self.mmc.loadSystemConfiguration(configfile)
+        self.dustAssayImagesRoot = 'x:\ATDB\images\dustassay'
+        self.cnx = mysql.connector.connect(user='atdb_client', password='atdb123', host='127.0.0.1', database='atdb')
+        self.csIDList.itemClicked.connect(self.Clicked)
+        self.populateUI()
+
+    def initUI(self):
+        currpath=os.path.split(os.path.realpath(__file__))[0]
+        filename = os.path.join(currpath,'csDustAssayUI.ui')
+        uic.loadUi(filename,self)
+
+        self.img1 = pg.ImageItem()
+        self.p1 = self.image1.addPlot()
+        self.p1.addItem(self.img1)
+        self.img2 = pg.ImageItem()
+        self.p2 = self.image2.addPlot()
+        self.p2.addItem(self.img2)
+
+    def Clicked(self,item):
+        print "Current coverslip ID: " + item.text()
+
+    def populateUI(self):
+        self.loadCoverSlipIDS()
+
+    def loadCoverSlipIDS(self):
+        cursor = self.cnx.cursor()
+        q = "SELECT id, status FROM coverslip ORDER by id"
+        cursor.execute(q)
+        rows = cursor.fetchall()
+        for row in rows:
+            self.csIDList.addItem(str(row[0]) + ',' + str(row[1]))
+
+    def populateDB(self):
+        dustAssay = ("INSERT INTO coverslipdustassays"
+                    "(coverslip_id, background_image, coverslip_image, coverslip_status)"
+                    "VALUES (%s, %s, %s, %s)")
+        dbData = []
+        csID = self.csIDList.currentItem().text()
+        dbData.append(str(csID[0]))
+        dbData.append(self.img1FileName)
+        dbData.append(self.img2FileName)
+        dbData.append(str(csID[2]))
+
+        try:
+            cursor = self.cnx.cursor()
+            cursor.execute(dustAssay, dbData)
+            newID = cursor.lastrowid
+            print 'Inserted assay: ' + `newID`
+            self.cnx.commit()
+            cursor.close()
+
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+
+    def csIDListClicked(self):
+        print 'Current id is:' + self.c
+
+
+    def dopush1(self):
+        print "hello"
+        self.mmc.snapImage()
+        data = self.mmc.getImage()
+        print data.shape
+        print data.dtype
+        self.img1.setImage(data, autoLevels=False)
+        self.img1FileName = datetime.now().strftime('%Y-%m-%d')
+
+    def dopush2(self):
+        print "hello 22"
+        self.mmc.snapImage()
+        data = self.mmc.getImage()
+        print data.shape
+        print data.dtype
+        self.img2.setImage(data, autoLevels=False)
+        self.img2FileName = datetime.now().strftime('%Y-%m-%d')
+
+    def saveBtnClick(self):
+        print "Populate DB and save files"
+        print os.path.join(self.dustAssayImagesRoot, self.img1FileName)
+
+        path, dirs, files = os.walk(self.dustAssayImagesRoot).next()
+        file_count = len(files)
+
+        self.img1FileName = self.img1FileName + '_' + str(file_count + 1) + '.jpg'
+        self.img2FileName = self.img2FileName + '_' + `file_count + 2` + '.jpg'
+
+        img1FName = os.path.join(self.dustAssayImagesRoot, self.img1FileName)
+        img2FName = os.path.join(self.dustAssayImagesRoot, self.img2FileName)
+
+        self.img1.save(img1FName)
+        self.img2.save(img2FName)
+        self.populateDB()
+
+
+    def run(self):
+        self.show()
+        qtapp.exec_()
+
+if __name__ == '__main__':
+
+     qtapp = QtGui.QApplication(sys.argv)
+     configfile= 'P:\\QtDemo\\ThorLabCameraOnly.cfg'
+     app = MyWidget(configfile)
+     app.run()
